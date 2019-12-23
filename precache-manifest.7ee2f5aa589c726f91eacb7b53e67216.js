465a87b4f3d75cdb49ac443bdaf83d7f7fe0b275
Bohdan <bohdan.romashchenko@gmail.com>
2019-12-24 01:27:08 +0200

    Updates

--- precache-manifest.7ee2f5aa589c726f91eacb7b53e67216.js
+++ precache-manifest.7ee2f5aa589c726f91eacb7b53e67216.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ece0dc5f30969a1b7789fa07a1c92d79",
+    "revision": "244a8843aeedf03cc885759681334e7b",
     "url": "/face-recognition-webapp-front/index.html"
   },
   {
-    "revision": "8a90262c83708ec582da",
+    "revision": "402341c11cde13a05111",
     "url": "/face-recognition-webapp-front/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "354bcdbcb37f772af9fc",
+    "revision": "05dc476704e7830f0397",
     "url": "/face-recognition-webapp-front/static/css/main.2262506c.chunk.css"
   },
   {
-    "revision": "8a90262c83708ec582da",
-    "url": "/face-recognition-webapp-front/static/js/2.44f51025.chunk.js"
+    "revision": "402341c11cde13a05111",
+    "url": "/face-recognition-webapp-front/static/js/2.384269d4.chunk.js"
   },
   {
     "revision": "c6d01cb19213691bc81a274434d00fe7",
-    "url": "/face-recognition-webapp-front/static/js/2.44f51025.chunk.js.LICENSE"
+    "url": "/face-recognition-webapp-front/static/js/2.384269d4.chunk.js.LICENSE"
   },
   {
-    "revision": "354bcdbcb37f772af9fc",
-    "url": "/face-recognition-webapp-front/static/js/main.0e313e04.chunk.js"
+    "revision": "05dc476704e7830f0397",
+    "url": "/face-recognition-webapp-front/static/js/main.1e55a5ca.chunk.js"
   },
   {
     "revision": "bd87b973c5ae8b59a0a3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/face-recognition-webapp-front/precache-manifest.842c811f9ef098aef8e67b1bce9ef227.js"
+  "/face-recognition-webapp-front/precache-manifest.7ee2f5aa589c726f91eacb7b53e67216.js"
 );
 
 self.addEventListener('message', (event) => {
