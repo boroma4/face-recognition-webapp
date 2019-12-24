85e1eb3f0f0f7392fecaedf1b50541ace6a6e256
Bohdan <bohdan.romashchenko@gmail.com>
2019-12-24 18:36:44 +0200

    add example

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,14 +2,21 @@ import React from "react";
 import './ImageLinkForm.css'
 
 const ImageLinkForm = ({onInputChange,onSubmit}) => {
+    const onExampleClick = () => {
+        document.getElementById('input-field').setAttribute('value','https://i.ibb.co/jwjnx7Z/7723cf7b-2c0f-41be-b540-dd18c32e03f5.jpg');
+        document.getElementById('input-field').dispatchEvent(new Event('input', { bubbles: true }))
+
+
+    };
     return(
         <div>
             <p className='f3'>
-                Magic
+                Put a link to an image and see what happens
             </p>
+            <p id='ex' onClick={onExampleClick}><u>Example</u></p>
             <div className='center'>
                 <div className='pa4 br3 shadow-5 center form'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                    <input className='f4 pa2 w-70 center' id='input-field' type='text' onChange={onInputChange} />
                     <button
                         onClick={onSubmit}
                         className='w-30 grow f4 link ph3 pv2 dib white bg-black'>Detect</button>
